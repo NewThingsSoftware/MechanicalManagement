d8a1431b593cdc9655d4476eef622862b71b91f7
Bruno <Bruno@BrunoPC.unoesc.lan>
2013-04-13 16:37:36 -0300

    Bruno

--- MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
+++ MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
@@ -6,7 +6,6 @@ import entidades.Cliente;
 public class MechanicalManagement {
 
     public static void main(String[] args) {
-       Cliente c = VeiculoDAO.obterPorModelo("Vectra").get(0).getCliente();
-        System.out.println(c.getNome());
+        
     }
 }
