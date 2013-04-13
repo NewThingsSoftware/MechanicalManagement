1dbad1dc0e725bcc511ee3cdc317cb04b9d7f30c
Marihelly Santini <marihelly_santini@hotmail.com>
2013-04-13 16:43:16 -0300

    Marihelly

--- MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
+++ MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
@@ -3,11 +3,12 @@ package mechanicalmanagement;
 import dao.ClienteDAO;
 import dao.VeiculoDAO;
 import entidades.Cliente;
+import entidades.Veiculo;
 
 public class MechanicalManagement {
 
     public static void main(String[] args) {
-        Cliente c = new Cliente("Teste", 444, 0004, true);
-        ClienteDAO.gravar(c);
+       Cliente cliente = VeiculoDAO.obterPorMarca("honda").get(0).getCliente();
+        System.out.println(cliente.getNome());
     }
 }
