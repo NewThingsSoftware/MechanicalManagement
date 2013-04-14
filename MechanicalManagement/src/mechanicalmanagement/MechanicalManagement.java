759a07f51f663ba4451cf2ccdbdb1384f998da2d
Bruno <Bruno@BrunoPC.unoesc.lan>
2013-04-13 22:39:52 -0300

    Tutorial

--- MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
+++ MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
@@ -1,14 +1,11 @@
 package mechanicalmanagement;
 
-import dao.ClienteDAO;
-import entidades.Cliente;
 
 
 public class MechanicalManagement {
 
     public static void main(String[] args) {
         TelaCentral telaCentral = new TelaCentral();
-        telaCentral.veiculo.setVisible(true);
-        //Cliente cliente = ClienteDAO.obertPorNome("Mari").get(0);
+        telaCentral.setVisible(true);
     }
 }
