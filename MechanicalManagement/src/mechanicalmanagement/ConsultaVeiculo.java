13adac32d3c774a8ee7ff561e3494091463d18f5
Bruno <Bruno@BrunoPC.unoesc.lan>
2013-04-14 23:29:21 -0300

    Consulta de Veiculo completa

--- MechanicalManagement/src/mechanicalmanagement/ConsultaVeiculo.java
+++ MechanicalManagement/src/mechanicalmanagement/ConsultaVeiculo.java
@@ -4,13 +4,11 @@
  */
 package mechanicalmanagement;
 
+import TableModels.VeiculoTableModel;
 import dao.VeiculoDAO;
 import entidades.Veiculo;
-import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
 public class ConsultaVeiculo extends javax.swing.JFrame {
 
@@ -21,32 +19,13 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
 
     public ConsultaVeiculo() {
         initComponents();
-        veiculoList.removeAll(veiculoList);
-        veiculoList.addAll(veiculoQuery.getResultList());
+        jTable1.setModel(new VeiculoTableModel(VeiculoDAO.obterTodos()));
     }
 
     public ConsultaVeiculo(CadastroDeVeiculos cadastroDeVeiculos) {
         this.cadastroDeVeiculos = cadastroDeVeiculos;
         initComponents();
-        veiculoList.removeAll(veiculoList);
-        veiculoList.addAll(veiculoQuery.getResultList());
-    }
-
-    /*Metodo para preencher a tabela com a consulta feita*/
-    private TableModel preencheTabela(List<Veiculo> veiculos) {
-        TableModel tableModel = jTable1.getModel();
-        
-        System.out.println(
-        tableModel.getRowCount());
-        for (int i = 0; i < veiculos.size(); i++) {
-            System.out.println(i);
-            tableModel.setValueAt(veiculos.get(i).getPlaca(), i, 0);
-            tableModel.setValueAt(veiculos.get(i).getModelo(), i, 1);
-            tableModel.setValueAt(veiculos.get(i).getMarca(), i, 2);
-            tableModel.setValueAt(veiculos.get(i).getAno(), i, 3);
-            tableModel.setValueAt(veiculos.get(i).getKm(), i, 4);
-        }
-        return tableModel;
+        jTable1.setModel(new VeiculoTableModel(VeiculoDAO.obterTodos()));
     }
 
     /**
@@ -57,66 +36,56 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
-        entityManager = java.beans.Beans.isDesignTime() ? null : javax.persistence.Persistence.createEntityManagerFactory("mecanica?zeroDateTimeBehavior=convertToNullPU").createEntityManager();
-        veiculoQuery = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT v FROM Veiculo v");
-        veiculoList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : org.jdesktop.observablecollections.ObservableCollections.observableList(veiculoQuery.getResultList());
-        veiculoQueryPlaca = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT v FROM Veiculo v");
+        jBGconsulta = new javax.swing.ButtonGroup();
         jBselecionar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jTFplaca = new javax.swing.JTextField();
-        jBconsultar = new javax.swing.JButton();
+        jBprocurar = new javax.swing.JButton();
+        jRBplaca = new javax.swing.JRadioButton();
+        jRBmarca = new javax.swing.JRadioButton();
+        jRBmodelo = new javax.swing.JRadioButton();
+        jRBtodos = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
 
-        jBselecionar.setText("jButton1");
+        jBselecionar.setText("Selecionar");
         jBselecionar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBselecionarActionPerformed(evt);
             }
         });
 
-        org.jdesktop.swingbinding.JTableBinding jTableBinding = org.jdesktop.swingbinding.SwingBindings.createJTableBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, veiculoList, jTable1);
-        org.jdesktop.swingbinding.JTableBinding.ColumnBinding columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${placa}"));
-        columnBinding.setColumnName("Placa");
-        columnBinding.setColumnClass(String.class);
-        columnBinding.setEditable(false);
-        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${modelo}"));
-        columnBinding.setColumnName("Modelo");
-        columnBinding.setColumnClass(String.class);
-        columnBinding.setEditable(false);
-        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${marca}"));
-        columnBinding.setColumnName("Marca");
-        columnBinding.setColumnClass(String.class);
-        columnBinding.setEditable(false);
-        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${ano}"));
-        columnBinding.setColumnName("Ano");
-        columnBinding.setColumnClass(Integer.class);
-        columnBinding.setEditable(false);
-        columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${km}"));
-        columnBinding.setColumnName("Km");
-        columnBinding.setColumnClass(Integer.class);
-        columnBinding.setEditable(false);
-        bindingGroup.addBinding(jTableBinding);
-        jTableBinding.bind();
         jScrollPane1.setViewportView(jTable1);
 
-        jBconsultar.setText("jButton2");
-        jBconsultar.addActionListener(new java.awt.event.ActionListener() {
+        jBprocurar.setText("Procurar");
+        jBprocurar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBconsultarActionPerformed(evt);
+                jBprocurarActionPerformed(evt);
             }
         });
+
+        jBGconsulta.add(jRBplaca);
+        jRBplaca.setText("Placa");
+
+        jBGconsulta.add(jRBmarca);
+        jRBmarca.setText("Marca");
+
+        jBGconsulta.add(jRBmodelo);
+        jRBmodelo.setText("Modelo");
+
+        jBGconsulta.add(jRBtodos);
+        jRBtodos.setSelected(true);
+        jRBtodos.setText("Todos");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(302, Short.MAX_VALUE)
+                .addContainerGap(294, Short.MAX_VALUE)
                 .addComponent(jBselecionar)
                 .addGap(25, 25, 25))
             .addGroup(layout.createSequentialGroup()
@@ -129,30 +98,44 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jTFplaca, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jBconsultar)
+                        .addComponent(jBprocurar)
                         .addGap(20, 20, 20))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addComponent(jRBplaca)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRBmarca)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRBmodelo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jRBtodos)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBconsultar)
+                    .addComponent(jBprocurar)
                     .addComponent(jTFplaca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(8, 8, 8)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jRBplaca)
+                    .addComponent(jRBmarca)
+                    .addComponent(jRBmodelo)
+                    .addComponent(jRBtodos))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jBselecionar)
                 .addContainerGap())
         );
 
-        bindingGroup.bind();
-
         java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds((screenSize.width-416)/2, (screenSize.height-339)/2, 416, 339);
+        setBounds((screenSize.width-416)/2, (screenSize.height-377)/2, 416, 377);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBselecionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBselecionarActionPerformed
-        if (jTable1.getSelectedRow() > 0) {
+        if (jTable1.getSelectedRow() > -1) {
             String placa = jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString();
             Veiculo veiculo = VeiculoDAO.obterPorPlaca(placa).get(0);
             cadastroDeVeiculos.consultaVeiculo(veiculo);
@@ -162,14 +145,24 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jBselecionarActionPerformed
 
-    private void jBconsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBconsultarActionPerformed
-        veiculoList.removeAll(veiculoList);
-        jTable1.setModel(preencheTabela(VeiculoDAO.obterPorPlaca(jTFplaca.getText())));
-//        veiculoList.removeAll(veiculoList);
-//        veiculoQueryPlaca = entityManager.createQuery("SELECT v FROM Veiculo v "
-//                + "WHERE v.placa LIKE '%" + jTFplaca.getText() + "%'");
-//        veiculoList.addAll(veiculoQueryPlaca.getResultList());
-    }//GEN-LAST:event_jBconsultarActionPerformed
+    private void jBprocurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBprocurarActionPerformed
+        if (jRBmarca.isSelected()) {
+            List<Veiculo> veiculos = VeiculoDAO.obterPorMarca(jTFplaca.getText());
+            jTable1.setModel(new VeiculoTableModel(veiculos));
+        }
+        if (jRBmodelo.isSelected()) {
+            List<Veiculo> veiculos = VeiculoDAO.obterPorModelo(jTFplaca.getText());
+            jTable1.setModel(new VeiculoTableModel(veiculos));
+        }
+        if (jRBplaca.isSelected()) {
+            List<Veiculo> veiculos = VeiculoDAO.obterPorPlaca(jTFplaca.getText());
+            jTable1.setModel(new VeiculoTableModel(veiculos));
+        }
+        if (jRBtodos.isSelected()) {
+            List<Veiculo> veiculos = VeiculoDAO.obterTodos();
+            jTable1.setModel(new VeiculoTableModel(veiculos));
+        }
+    }//GEN-LAST:event_jBprocurarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -207,15 +200,15 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.persistence.EntityManager entityManager;
-    private javax.swing.JButton jBconsultar;
+    private javax.swing.ButtonGroup jBGconsulta;
+    private javax.swing.JButton jBprocurar;
     private javax.swing.JButton jBselecionar;
+    private javax.swing.JRadioButton jRBmarca;
+    private javax.swing.JRadioButton jRBmodelo;
+    private javax.swing.JRadioButton jRBplaca;
+    private javax.swing.JRadioButton jRBtodos;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTFplaca;
     private javax.swing.JTable jTable1;
-    private java.util.List<mechanicalmanagement.Veiculo> veiculoList;
-    private javax.persistence.Query veiculoQuery;
-    private javax.persistence.Query veiculoQueryPlaca;
-    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
