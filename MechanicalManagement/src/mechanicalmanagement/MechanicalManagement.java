a2a8bc61ae3cb46995580f6fc88cc6a0ae67545c
ctb03 <ctb03@WS-01-24>
2013-04-15 11:35:05 -0300

    Criada a classe OrdemServicoTableModel

--- MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
+++ MechanicalManagement/src/mechanicalmanagement/MechanicalManagement.java
@@ -5,7 +5,6 @@ public class MechanicalManagement {
 
     public static void main(String[] args) {
         TelaCentral telaCentral = new TelaCentral();
-        telaCentral.veiculo.setVisible(true);
-        //Cliente cliente = ClienteDAO.obertPorNome("Mari").get(0);
+        telaCentral.setVisible(true);
     }
 }
