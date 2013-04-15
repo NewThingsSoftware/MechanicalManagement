5a28cc86581d0403461f387128555f0cf1645709
ctb03 <ctb03@WS-01-24>
2013-04-15 16:17:13 -0300

    Alterado MecanicoDAO

--- MechanicalManagement/src/dao/MecanicoDAO.java
+++ MechanicalManagement/src/dao/MecanicoDAO.java
@@ -53,11 +53,43 @@ public class MecanicoDAO {
         return mecanicos;
     }
 
+    public static List<Mecanico> obterPorCpf(int cpf) {
+        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.cpf = :cpf");
+        qry.setParameter("cpf", cpf );
+        List<Mecanico> mecanicos = qry.list();
+        return mecanicos;
+    }
+    public static List<Mecanico> obterPorRg(int rg) {
+        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.rg = :rg");
+        qry.setParameter("rg", rg);
+        List<Mecanico> mecanicos = qry.list();
+        return mecanicos;
+    }
+    public static List<Mecanico> obterPorTelefone(String telefone) {
+        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.telefone LIKE :telefone");
+        qry.setParameter("telefone", "%" + telefone + "%");
+        List<Mecanico> mecanicos = qry.list();
+        return mecanicos;
+    }
     public static List<Mecanico> obterPorEspecialidade(String especialidade) {
         Session s = HibernateUtil.getSessionFactory().getCurrentSession();
         s.beginTransaction();
-        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.especialidade = :especilidade");
-        qry.setParameter("especialidade", especialidade);
+        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.especialidade LIKE :especilidade");
+        qry.setParameter("especialidade", "%" + especialidade + "%");
+        List<Mecanico> mecanicos = qry.list();
+        return mecanicos;
+    }
+    public static List<Mecanico> obterPorStatus(boolean status) {
+        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        Query qry = s.createQuery("SELECT m FROM Mecanico m WHERE m.status = :status");
+        qry.setParameter("status", status);
         List<Mecanico> mecanicos = qry.list();
         return mecanicos;
     }
